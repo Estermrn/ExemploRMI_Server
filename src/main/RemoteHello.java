5dd58960ea2f84642e513151c4ed65c0037d8e1f
Estermrn <darosaester97@gmail.com>
2025-10-29 16:47:21 -0300

    :tada: Commit inicial

---
+++ src/main/RemoteHello.java
@@ -0,0 +1,9 @@
+package main;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface RemoteHello extends Remote {
+    public String getMensagem() throws RemoteException;
+    public void setMensagem(String mensagem) throws RemoteException;
+}
